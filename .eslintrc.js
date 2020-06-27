886735aa482c10709f7b443a4c1686cf4c3061a5
henrique-manjaro <henriqueoliwork@gmail.com>
2020-06-26 23:08:24 -0300

    add eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    reportUnusedDisableDirectives: true,
+    root: true,
+    env: {
+        browser: true,
+        es2020: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard',
+        '@vue/typescript',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: [
+        'vue',
+        '@typescript-eslint'
+    ],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'always'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/ban-types': 'off'
+    },
+    overrides: [
+        {
+            files: [
+                '**/*.test.ts'
+            ],
+            env: {
+                jest: true
+            },
+            plugins: ['jest']
+        }
+    ]
+};
