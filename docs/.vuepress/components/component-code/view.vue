ae0a4f7ea453ebc51c0cfe754f4cfdde6ac071e1
henrique-manjaro <henriqueoliwork@gmail.com>
2020-07-05 01:10:37 -0300

    docs: create <component-code-view />

---
+++ docs/.vuepress/components/component-code/view.vue
@@ -0,0 +1,45 @@
+<template>
+    <w-card>
+      <div class="flex border-b">
+        <template v-for='(item, index) in items'>
+          <w-btn
+            v-if="$slots[item]"
+            :key="index"
+            v-text="capitalize(item)"
+            :text='index !== tab'
+            @click='tab = index'
+          />
+        </template>
+        </div>
+      </div>
+      <w-card-content>
+        <slot />
+        <template v-for='(item, index) in items'>
+          <div :key="index" v-if='index === tab && $slots[item]'>
+            <slot :name="item"/>
+          </div>
+        </template>
+      </w-card-content>
+    </w-card>
+</template>
+
+<script>
+import capitalize from 'lodash/capitalize';
+export default {
+  data(){
+    return {
+      tab: 0,
+      capitalize,
+      items: [
+        "template",
+        "script",
+        "typescript"
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
