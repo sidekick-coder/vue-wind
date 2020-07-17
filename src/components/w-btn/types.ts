a56c0012a3114cc72267d755f2333b7879f83189
henrique-manjaro <henriqueoliwork@gmail.com>
2020-07-16 22:22:20 -0300

    w-btn: add unit tests

---
+++ src/components/w-btn/types.ts
@@ -0,0 +1,10 @@
+export interface Props {
+    defaultClass: string | string[];
+    color: string;
+    text: boolean;
+    block: boolean;
+    rounded: boolean;
+    loading: boolean;
+    spinSize: string;
+    spinSeep: string;
+}
