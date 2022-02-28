0042282930eb3cd0ecd9c8376b3f3c8c79829820
Henrique <henryqueoli@hotmail.com>
2022-02-28 14:19:02 -0300

    refactor: use class buillder in w-card

--- src/components/w-card/w-card.vue
+++ src/components/w-card/w-card.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import { useClassBuilder } from "@/composable/class-builder";
+
+const classes = useClassBuilder().add(
+    "shadow-md w-full rounded bg-white dark:bg-gray-800"
+);
+</script>
+
 <template>
-    <div class="shadow-md w-full rounded bg-white dark:bg-gray-800">
+    <div :class="classes.build()">
         <slot></slot>
     </div>
 </template>
