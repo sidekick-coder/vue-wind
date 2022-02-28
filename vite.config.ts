65ca9679ef3876dac82e8618050092dd704975f5
Henrique <henryqueoli@hotmail.com>
2022-02-28 14:18:41 -0300

    feat: add @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
     build: {
         lib: {
             entry: path.resolve(__dirname, "src", "index.ts"),
