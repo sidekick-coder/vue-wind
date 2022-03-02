09ad7e8a3193f47ceee9e71b4d8f81c0522e6754
Henrique <henryqueoli@hotmail.com>
2022-03-02 11:23:40 -0300

    tests:  add w-content tests

---
+++ src/components/w-content/w-content.spec.ts
@@ -0,0 +1,57 @@
+import { describe, it, assert, vi, afterEach } from "vitest";
+import { nextTick } from "vue";
+import { mount } from "@vue/test-utils";
+
+import WLayout from "@/components/w-layout/w-layout.vue";
+import WToolbar from "@/components/w-toolbar/w-toolbar.vue";
+import WContent from "./w-content.vue";
+
+describe("w-content", () => {
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it("should the height be 100% when toolbarRef not exist", async () => {
+        const component = {
+            components: { WContent, WLayout },
+            template: `<w-layout> <w-content layout></w-content>  </w-layout>`,
+        };
+
+        const wrapper = mount(component);
+
+        await nextTick();
+
+        const content = wrapper.findComponent(WContent);
+
+        assert.include(content.attributes("class"), "h-[100%]");
+    });
+
+    it("should height be screen height - toolbar height when toolbarRef is defined", async () => {
+        vi.spyOn(document.body, "clientHeight", "get").mockImplementation(
+            () => 1000
+        );
+
+        const component = {
+            components: { WLayout, WToolbar, WContent },
+            template: `
+                <w-layout>
+                    <w-toolbar layout />
+                    <w-content layout />
+                </w-layout>`,
+        };
+
+        const wrapper = mount(component);
+        const content = wrapper.findComponent(WContent);
+        const toolbar = wrapper.findComponent(WToolbar);
+
+        vi.spyOn(toolbar.element, "clientHeight", "get").mockImplementation(
+            () => 50
+        );
+
+        await nextTick();
+        await nextTick();
+        await nextTick();
+
+        assert.include(content.attributes("class"), "h-[950px]");
+    });
+});
