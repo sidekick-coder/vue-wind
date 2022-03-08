eb64166d5cc26d069f31a165ee31c2c79b201eff
Henrique <henryqueoli@hotmail.com>
2022-03-08 11:25:34 -0300

    feat: add width and max-width to w-card

--- src/components/w-card/w-card.spec.ts
+++ src/components/w-card/w-card.spec.ts
@@ -1,13 +1,10 @@
-import { describe, it, assert, afterEach, beforeEach } from "vitest";
+import { describe, it, assert, afterEach } from "vitest";
 import { mount, VueWrapper } from "@vue/test-utils";
 
 import WCard from "./w-card.vue";
+import { nextTick } from "process";
 
 let wrapper: VueWrapper;
-
-beforeEach(() => {
-    wrapper = mount(WCard);
-});
 
 afterEach(() => {
     wrapper.unmount();
@@ -15,12 +12,38 @@ afterEach(() => {
 
 describe("w-card", () => {
     it("should render the content", () => {
-        const wrapper = mount(WCard, {
+        wrapper = mount(WCard, {
             slots: {
                 default: "<div>Hello World</div>",
             },
         });
 
         assert.equal(wrapper.text(), "Hello World");
+    });
+
+    it("should use width prop in the class", () => {
+        wrapper = mount(WCard, {
+            props: {
+                width: "[500px]",
+            },
+        });
+
+        assert.include(wrapper.attributes("class"), "w-[500px]");
+    });
+
+    it("should not have max-width by default", () => {
+        wrapper = mount(WCard);
+
+        assert.notInclude(wrapper.attributes("class"), "max-w");
+    });
+
+    it("should use max-width class based in prop value", () => {
+        wrapper = mount(WCard, {
+            props: {
+                maxWidth: "[500px]",
+            },
+        });
+
+        assert.include(wrapper.attributes("class"), "max-w-[500px]");
     });
 });
--- src/components/w-card/w-card.vue
+++ src/components/w-card/w-card.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 import { useClassBuilder } from "@/composable/class-builder";
 
-const classes = useClassBuilder().add(
-    "shadow-md w-full rounded bg-white dark:bg-gray-800"
-);
+const props = defineProps({
+    width: {
+        type: String,
+        default: "full",
+    },
+    maxWidth: {
+        type: String,
+        default: null,
+    },
+});
+
+const builder = useClassBuilder();
+
+builder.add("shadow-md rounded");
+
+builder.add("bg-white dark:bg-gray-800");
+
+builder.add(`w-${props.width}`);
+
+if (props.maxWidth) {
+    builder.add(`max-w-${props.maxWidth}`);
+}
 </script>
 
 <template>
-    <div :class="classes.build()">
+    <div :class="builder.build()">
         <slot></slot>
     </div>
 </template>
