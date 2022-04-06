e6dd55af3b5efa23738d54257814ddfd56e20f6f
Henrique <henryqueoli@hotmail.com>
2022-04-06 12:42:12 -0300

    feat(w-btn):  add disabled styles

--- src/components/w-btn/w-btn.vue
+++ src/components/w-btn/w-btn.vue
@@ -9,6 +9,7 @@ builder
     .add("textColor", "text-", "white")
     .add("textSize", "text-", "base")
     .add("width", "w-", "full")
+    .addStatic("disabled:opacity-75")
     .addStatic("px-4 py-2 drop-shadow rounded font-medium");
 
 export default {
