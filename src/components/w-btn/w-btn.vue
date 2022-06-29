43020fdfc8aea4be6ab95e5ef5c68faa1d60b913
Henrique <henryqueoli@hotmail.com>
2022-06-29 08:20:12 -0300

    feat: add w-btn sizes

--- src/components/w-btn/w-btn.vue
+++ src/components/w-btn/w-btn.vue
@@ -5,19 +5,69 @@ import { useBuilder } from "@/composable/tailwind";
 export const builder = useBuilder();
 
 builder
-    .option("color", "bg", "gray-500")
+    .option("bgColor", "bg", "gray-500")
     .option("textColor", "text", "white")
     .option("textSize", "text", "base")
-    .option("width", "w", "full")
-    .static("disabled:opacity-75")
-    .static("px-4 py-2 drop-shadow rounded font-medium");
+    .option("borderColor", "border", "transparent")
+    .option("width", "w", "auto")
+    .option("px", "px", "4")
+    .option("py", "py", "2")
+    .static("disabled:opacity-75 border")
+    .static("drop-shadow rounded font-medium");
+
+export const sizes = {
+    sm: {
+        px: "4",
+        py: "2",
+        textSize: "xs",
+    },
+    md: {
+        px: "4",
+        py: "2",
+        textSize: "base",
+    },
+    lg: {
+        px: "4",
+        py: "2",
+        textSize: "lg",
+    },
+    xl: {
+        px: "4",
+        py: "2",
+        textSize: "xl",
+    },
+};
 
 export default defineComponent({
     props: {
         ...builder.props,
+        color: {
+            type: String,
+            default: undefined,
+        },
+        size: {
+            type: String,
+            default: "md",
+        },
+        sizes: {
+            type: Object,
+            default: () => sizes,
+        },
+        outlined: {
+            type: Boolean,
+            default: false,
+        },
     },
-    setup(props) {
-        const classes = computed(() => builder.make(props));
+    setup(props: any) {
+        const classes = computed(() =>
+            builder.make({
+                ...props,
+                ...props.sizes[props.size],
+                borderColor: props.outlined ? props.color : "transparent",
+                textColor: props.outlined ? props.color : props.textColor,
+                bgColor: props.outlined ? "transparent" : props.color,
+            })
+        );
 
         return { classes };
     },
