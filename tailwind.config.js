d02d2c397dafa83f13312cf70c23850ead4acc96
Henrique <henryqueoli@hotmail.com>
2022-08-11 15:15:33 -0300

    refactor: remove storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,9 @@
-const { transformer } = require("vue-wind");
 module.exports = {
     theme: {},
     content: {
         files: [
-            "./src/components/**/*.stories.ts",
             "./src/components/**/*.vue",
         ],
-        transform: {
-            ts: (content) => {
-                const vWindSafeList = transformer(content);
-
-                return [vWindSafeList, content].join("\n\n\n");
-            },
-        },
     },
-    safelist: [
-        "max-h-[200px]",
-        {
-            pattern: /bg-*/,
-            variants: ["focus", "placeholder-shown", "hover"],
-        },
-        {
-            pattern: /text-*/,
-            variants: ["focus", "placeholder-shown", "hover"],
-        },
-        {
-            pattern: /border-*/,
-            variants: ["focus", "placeholder-shown", "hover"],
-        },
-        {
-            pattern: /fill-*/,
-            variants: ["focus", "placeholder-shown", "hover"],
-        },
-        {
-            pattern: /opacity-*/,
-            variants: ["focus", "placeholder-shown", "hover"],
-        },
-    ],
     plugins: [],
 };
