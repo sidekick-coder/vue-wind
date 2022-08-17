d918e6c92bf4929b8aade60a218fa0e7d3dd5179
Henrique <henryqueoli@hotmail.com>
2022-08-17 14:30:33 -0300

    feat(docs): add layout docs

---
+++ packages/docs/content/components/w-layout.md
@@ -0,0 +1,12 @@
+---
+title: Layout
+---
+# Layout
+
+A set of components to define the app layout
+
+## Basic
+
+This example **use percentage** but the component use **window view ports** height & width
+
+:c-example{file="layout/basic"}
---
+++ packages/docs/examples/layout/basic.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-[500px]">
+        <w-layout use-percentage>
+            
+            <w-toolbar :layout-ignore="['left']" class="bg-red-500" >
+                <div class="text-white px-5 py-2">Toolbar</div>
+            </w-toolbar>
+    
+            <w-drawer class="bg-blue-500" layout-id="left">
+                <div class="text-white px-5 py-2">Drawer</div>
+            </w-drawer>
+    
+            <w-content>
+                <div class="w-full h-full flex bg-gray-500 text-white items-center justify-center">
+                    Content
+                </div>
+            </w-content>
+        </w-layout>
+    </div>
+</template>
