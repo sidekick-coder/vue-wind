32271424dba95c6f2a3f5abcf723fdd0110a3278
Henrique <henryqueoli@hotmail.com>
2023-05-09 13:43:00 -0300

    feat(composables): define-variation composable

--- packages/composables/src/define-variation.spec.ts
+++ packages/composables/src/define-variation.spec.ts
@@ -1,7 +1,130 @@
-import { describe, it, expect } from 'vitest'
+import { test, expect, describe } from 'vitest'
+import { reactive } from 'vue'
 
-describe('defineVariation', () => {
-    it('should be defined', () => {
-        expect(true).toBe(true)
+import { defineVariation } from './define-variation'
+
+describe('define-variation (unit)', () => {
+    test('should return correct variation classes', () => {
+        const props = { color: 'accent' }
+
+        const variant = defineVariation(props, 'color', {
+            accent: 'bg-accent',
+            info: 'bg-info',
+        })
+
+        expect(variant.classes).toBe('bg-accent')
+    })
+
+    test('should update variation classes', () => {
+        const props = reactive({ color: 'accent' })
+
+        const variant = defineVariation(props, 'color', {
+            accent: 'bg-accent',
+            info: 'bg-info',
+        })
+
+        expect(variant.classes).toBe('bg-accent')
+
+        props.color = 'info'
+
+        expect(variant.classes).toBe('bg-info')
+    })
+
+    test('should be able to use function as variation', () => {
+        const props = { color: 'accent' }
+
+        const variant = defineVariation(props, 'color', {
+            accent: () => ({ classes: 'bg-accent', styles: 'color: var(--accent)' }),
+        })
+
+        expect(variant.classes).toBe('bg-accent')
+        expect(variant.styles).toBe('color: var(--accent)')
+    })
+
+    test('should add shared classes between variations ', () => {
+        const props = reactive({ color: 'accent' })
+
+        const variant = defineVariation(props, 'color', {
+            _shared: 'border',
+            accent: 'bg-accent',
+            info: 'bg-info',
+        })
+
+        expect(variant.classes).toBe('border bg-accent')
+
+        props.color = 'info'
+
+        expect(variant.classes).toBe('border bg-info')
+    })
+
+    test('should add shared styles between variations ', () => {
+        const props = reactive({ color: 'accent' })
+
+        const variant = defineVariation(props, 'color', {
+            _shared: () => ({ styles: 'font-weight:bold;' }),
+            accent: () => ({ styles: 'color: var(--accent);' }),
+            info: () => ({ styles: 'color: var(--info);' }),
+        })
+
+        expect(variant.styles).toBe('font-weight:bold; color: var(--accent);')
+
+        props.color = 'info'
+
+        expect(variant.styles).toBe('font-weight:bold; color: var(--info);')
+    })
+
+    test('should use _empty classes when variation was not found ', () => {
+        const props = reactive({ color: 'unknown' })
+
+        const variant = defineVariation(props, 'color', {
+            _empty: 'text-gray',
+        })
+
+        expect(variant.classes).toBe('text-gray')
+    })
+
+    test('should use _empty styles when variation was not found ', () => {
+        const props = reactive({ color: 'unknown' })
+
+        const variant = defineVariation(props, 'color', {
+            _empty: () => ({ styles: 'color: grey' }),
+        })
+
+        expect(variant.styles).toBe('color: grey')
+    })
+
+    test('should update variations options', () => {
+        const props = reactive({ color: 'accent' })
+
+        const variant = defineVariation(props, 'color', {
+            _shared: 'border',
+            accent: 'bg-accent',
+            info: 'bg-info',
+        })
+
+        expect(variant.classes).toBe('border bg-accent')
+
+        variant.setOptions({
+            _shared: 'font-bold',
+            accent: 'text-accent',
+            info: 'text-info',
+        })
+
+        expect(variant.classes).toBe('font-bold text-accent')
+
+        props.color = 'info'
+
+        expect(variant.classes).toBe('font-bold text-info')
+    })
+
+    test('should use _shared & empty at same type options', () => {
+        const props = reactive({ color: 'text-unknown' })
+
+        const variant = defineVariation(props, 'color', {
+            _shared: 'border',
+            _empty: (v) => ({ classes: v }),
+        })
+
+        expect(variant.classes).toBe('border text-unknown')
     })
 })
--- packages/composables/vitest.config.ts
+++ packages/composables/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        reporters: 'verbose',
         cache: {
             dir: '.vitest'
         },
